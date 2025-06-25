fd8d138cf9a76289188d7f27bf24aaaf715e655d
Eike Thaden <ethaden@confluent.io>
2025-06-25 10:13:04 +0000

    Extended jwt decoder

--- python/src/decode_jwt.py
+++ python/src/decode_jwt.py
@@ -1,6 +1,7 @@
 import sys
 import jwt
 import argparse
+from datetime import datetime, tzinfo
 
 def decode_jwt(access_token: str):
     alg = jwt.get_unverified_header(access_token)['alg']
@@ -18,4 +19,11 @@ if __name__=='__main__':
     #parsed_args = parser.parse_args(args=sys.argv)
     parsed_args = parser.parse_args()
     access_token = parsed_args.access_token
-    print(decode_jwt(access_token))
+    decoded_token = decode_jwt(access_token)
+    print(decoded_token)
+    if 'iat' in decoded_token:
+        iat_date = datetime.fromtimestamp(decoded_token.get('iat'))
+        print(f'Issued at: {iat_date.strftime("%Y-%m-%d %H:%M:%S")} (UTC)')
+    if 'exp' in decoded_token:
+        exp_date = datetime.fromtimestamp(decoded_token.get('exp'))
+        print(f'Expiration at: {exp_date.strftime("%Y-%m-%d %H:%M:%S")} (UTC)')
